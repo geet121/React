208df14343f43c945859e1d1173d0996c8cf5aa8
rishi0395 <guptarishi64@gmail.com>
2020-04-06 01:00:48 +0530

    navItems added

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
 
 const toolbar = () => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
     <Logo />
-    <nav>...</nav>
+    <nav>
+      <NavItems />
+    </nav>
   </header>
 );
 
